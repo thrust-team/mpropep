ff9e0609c4bdd2ff1bfa10a5a25eefce445d112c
Alessandro Rampazzo <90860412+AleRampa@users.noreply.github.com>
2022-12-21 13:54:21 +0100

    added clc clear close all and changed substances

--- code/exampleGetFlameTemperature.m
+++ code/exampleGetFlameTemperature.m
@@ -1,12 +1,23 @@
-clear all; clc
+clc
+clear
+close all
 
-mkdir .mpropep
+addpath("cpropep")
+addpath("gasdynamics\")
+addpath("io\")
+addpath("num\")
+
+if ~exist(".mpropep","dir")
+    mkdir .mpropep
+   
+end
+
 mpropepPath = pwd;
 
-oxidantID = 657; % Dinitrogen monoxide
-fuelID = 1032; % Paraffin
-OFRatio = 7;
-pressure = 50e5;
+oxidantID = getID("oxygen (gas)"); % N2O
+fuelID = getID("methane"); % Paraffin 0907 (C50H102)
+OFRatio = 2;
+pressure = 1e5;
 expansionRatio = 5;
 listPropellantID = [oxidantID fuelID];
 listMass = [OFRatio 1]*1e-3;
