c5078273e91f66f9dadab84154481d5cdfac4eb3
Alberto <apmox21@gmail.com>
2023-01-24 23:32:32 +0100

    test suite now does something
    many voices do not match though

--- code/tests/testIOScripts.m
+++ code/tests/testIOScripts.m
@@ -1,19 +1,23 @@
 %% getID and getPropellantFromID test
 % This function assesses if both functions work as expected
 
+clc; clear all;
+
 addpath("../io")
 
-propellantID = 0;
+propellantID = -1;
 while true
     propellantID = propellantID + 1;
-    
-    disp(propellantID);
     propellantName = getPropellantFromID(propellantID);
-    disp(propellantName)
-    propellantIDcheck = getID(propellantName);
-    disp(propellantIDcheck);
-    disp(propellantIDcheck == propellantID);
-    if propellantID == 1000
-        break;
+    propellantIDCheck = getID(propellantName);
+      
+    % fprintf("%i %s %i\n", propellantID, propellantName, propellantIDCheck);
+    if ~(propellantIDCheck == propellantID)
+        fprintf("ID: %i - %s did not match\n", propellantID, propellantName)
+    end
+    
+    % TODO: 1st commandment is never to hardcode stuff
+    if propellantID == 1051
+        break
     end
 end
