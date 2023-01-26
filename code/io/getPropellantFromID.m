ebc8ac415b919a3d6517a28aff5366b010dbff46
Alberto <apmox21@gmail.com>
2023-01-26 21:33:31 +0100

    small addition to comments, whitespace

--- code/io/getPropellantFromID.m
+++ code/io/getPropellantFromID.m
@@ -1,16 +1,15 @@
 % Function to find the name of a compound given its identification number
 % interpreted by cpropep from propellant.dat file.
 % This is useful because usually the ID is passed as a parameter in most
-% of the mpropep library.
+% of the mpropep library. Using ID is better than name since sometimes
+% the name is ambiguous.
 % Note that the ID is different than the number in the propellant.dat
 % text file.
-% Some compounds get their name cut because they are multiline in
-% propellant.dat.
 
 
 function propellantName = getPropellantFromID(propellantID)
-
     addpath("../cpropep/")
+    
     % Check that list.txt is in the folder, otherwise generate it.
     fid = fopen('cpropep/list.txt', 'r');
     if fid == -1
@@ -30,5 +29,4 @@ function propellantName = getPropellantFromID(propellantID)
     output = char(scan{1}(propellantID + 3));
     strings = split(output);
     propellantName = lower(char(join(strings(2:end-1),' ')));
-    
 end
