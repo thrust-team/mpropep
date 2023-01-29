ea00f2d26e5c565b360e1e247c27935784820c6c
Alessandro Rampazzo <alessandro.rampazzo.10@studenti.unipd.it>
2023-01-29 12:01:58 +0100

    bug fix
    bug fix regarding multiple substances having the same name

--- code/tests/testIOScripts.m
+++ code/tests/testIOScripts.m
@@ -5,20 +5,35 @@ clc; clear all;
 
 addpath("../io")
 
-propellantID = -1;
+propellantID = 1000;
+good = true;
+substancesWithAliases = string([]);
+disp("checking ID...")
 while true
+    
     propellantID = propellantID + 1;
+%     fprintf("ID: %i - ok\n",propellantID)
     propellantName = getPropellantFromID(propellantID);
     propellantIDCheck = getID(propellantName);
       
     % fprintf("%i %s %i\n", propellantID, propellantName, propellantIDCheck);
     if ~(propellantIDCheck == propellantID)
-        fprintf("ID: %i - %s did not match\n", propellantID, propellantName)
+        good = false;
+        substancesWithAliases(end+1) = propellantName;
+        warning("ID: %i - %s did not match\n", propellantID, propellantName)
     end
     
     % TODO: 1st commandment is never to hardcode stuff, yet here i am
-    % transgressing
+    % transgressing - lol
     if propellantID == 1051
         break
     end
+end
+if ~good
+    for i = 1:length(substancesWithAliases)
+        substancesWithAliases(i) = " - " + substancesWithAliases(i)+newline;
+    end
+    warning("these substances have two or more aliases: \n%s",join(substancesWithAliases,''))
+else
+    fprintf("no substances have aliases\n")
 end
